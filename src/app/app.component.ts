7a833987d41a74259f2c46cce1c3dc73428c2b80
Jose Nortes <nooors@gmail.com>
2024-03-26 12:09:16 +0100

    feat: store initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,15 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ngrx-signal-store';
+export class AppComponent implements OnInit {
+  public store = Inject('TodosStore');
+
+  /**
+   * Angular lifecycle hook: Initialization
+   */
+  ngOnInit(): void {
+    this.store.todos();
+  }
 }
